3b6d8c02d06379402919c6f71330e4539458a1f3
k3nudo <k3nudo@gmail.com>
2025-05-29 21:37:14 +0200

    Neighborhood determination test

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 #include "nisza.h"
 #include "osobniki.h"
+#include "sasiedztwo.h"
 
 using namespace std;
 
-//globalne nisze oraz separator
-static Nisza n1, n2, n3;
-static char sep = UstawieniaSymulacji::pobierzUstawienia().znakSeparator;
-
-void wyswietlNisze() {
-    cout << n1.jakiSymbol() << sep
-            << n2.jakiSymbol() << sep
-            << n3.jakiSymbol() << endl;
-}
 
 int main() {
-    cout << "Puste nisze: ";
-    wyswietlNisze();
+    Nisza nisza;
+    nisza.przyjmijLokatora(new Bakteria());
 
-    cout << "Lokowanie mieszkancow: ";
-    n1.przyjmijLokatora(new Glon());
-    n3.przyjmijLokatora(new Grzyb());
-    wyswietlNisze();
+    cout << "Kto w niszy: "
+            << nisza.ktoTuMieszka() << endl;
+    cout << "Czy zywy: "
+            << nisza.lokatorZywy() << endl;
 
-    cout << "Przesuwanie lokatorow: ";
-    n2 = n1;
-    wyswietlNisze();
+    sasiedztwo sasiedztwo;
 
-    cout << "Przesuwanie lokatorow: ";
-    n3 = n2;
-    wyswietlNisze();
+    sasiedztwo.okreslSasiada(P, GLON);
+    sasiedztwo.okreslSasiada(PG, GRZYB);
+    sasiedztwo.okreslSasiada(G, GRZYB);
+    sasiedztwo.okreslSasiada(LG, GLON);
+    sasiedztwo.okreslSasiada(L, BAKTERIA);
+    sasiedztwo.okreslSasiada(LD, BAKTERIA);
+    sasiedztwo.okreslSasiada(D, GLON);
+    sasiedztwo.okreslSasiada(PD, PUSTKA);
 
+    ZamiarMieszkanca zamiar = nisza.aktywujLokatora(sasiedztwo);
+
+    cout << "Akcja: " << zamiar.akcja << " gdzie: " << zamiar.gdzie << endl;
     cout << endl;
+
+    return 0;
+
+
     return 0;
 }
