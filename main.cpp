3dc6624fff5230778b53a80f8663c570361184e4
k3nudo <k3nudo@gmail.com>
2025-05-29 21:12:37 +0200

    Test nisza class in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
-#include "Organizm.h"
-#include "generatorlosowy.h"
-#include "sasiedztwo.h"
-
+#include "nisza.h"
+#include "osobniki.h"
 
 using namespace std;
 
+//globalne nisze oraz separator
+static Nisza n1, n2, n3;
+static char sep = UstawieniaSymulacji::pobierzUstawienia().znakSeparator;
 
-int main()
-{
+void wyswietlNisze() {
+    cout << n1.jakiSymbol() << sep
+            << n2.jakiSymbol() << sep
+            << n3.jakiSymbol() << endl;
+}
+
+int main() {
+    cout << "Puste nisze: ";
+    wyswietlNisze();
+
+    cout << "Lokowanie mieszkancow: ";
+    n1.przyjmijLokatora(new Glon());
+    n3.przyjmijLokatora(new Grzyb());
+    wyswietlNisze();
+
+    cout << "Przesuwanie lokatorow: ";
+    n2 = n1;
+    wyswietlNisze();
+
+    cout << "Przesuwanie lokatorow: ";
+    n3 = n2;
+    wyswietlNisze();
+
+    cout << endl;
     return 0;
 }
