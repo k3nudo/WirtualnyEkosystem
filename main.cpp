def6ce5e1b76cea4d02a6b137188ef631d1ef79e
k3nudo <k3nudo@gmail.com>
2025-06-03 11:27:24 +0200

    File input and output for simulation

--- main.cpp
+++ main.cpp
@@ -6,40 +6,32 @@
 
 using namespace std;
 
-int main()
-{
-    srodowisko ekoSystem(8, 12);
-
-    ekoSystem.lokuj(new Glon(), 0, 10);
-    ekoSystem.lokuj(new Glon(), 1, 10);
-    ekoSystem.lokuj(new Glon(), 1, 13);
-    ekoSystem.lokuj(new Glon(), 3, 10);
-    ekoSystem.lokuj(new Grzyb(), 1, 11);
-    ekoSystem.lokuj(new Grzyb(), 0, 0);
-    ekoSystem.lokuj(new Bakteria(), 3, 3);
-    ekoSystem.lokuj(new Bakteria(), 2, 6);
+int main() {
+    srodowisko ekoSystem = srodowisko::czytajZPliku("start.txt");
 
     ofstream plikWynikowy("symulacja.txt");
     if (!plikWynikowy.is_open()) return 1;
-    string stanSymulacji;
 
+    string stanSymulacji;
     unsigned long i = 0;
 
     do {
         system("clear");
-        cout << "Krok symulacji: " << i << endl;
+        std::cout << "Krok symulacji: " << i << endl;
         plikWynikowy << "Krok symulacji: " << i << endl;
+
         stanSymulacji = ekoSystem.doTekstu();
 
         cout << endl << stanSymulacji << endl;
         plikWynikowy << stanSymulacji << endl;
+
         cin.ignore(1);
         ekoSystem++;
         i++;
     } while (i < 200 && !ekoSystem);
 
     cout << endl;
-
     plikWynikowy.close();
+
     return 0;
 }
